cf323912e0fd644532c666aeff398f13b375290a
Parkjindream <fhfg09820@gmail.com>
2026-09-21 23:29:04 +0700

    frist commmit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("bth-one").addEventListener('click',() =>{
+   alert("เริ่มการดาวโหลด!")
+})
+document.getElementById("bth-two").addEventListener('click',() =>{
+   alert("เริ่มการดาวโหลด!")
+})
+document.getElementById("bth-three").addEventListener('click',() =>{
+   alert("เริ่มการดาวโหลด!")
+})
+document.getElementById("bth-four").addEventListener('click',() =>{
+   alert("เริ่มการดาวโหลด!")
+})
+function myClick3(){
+    window.open('https://www.instagram.com/park_jindream/?hl=en')
+}
+function myClick1(){
+    window.open('https://web.facebook.com/anawin.boonchuay.341221/?locale=th_TH')
+}
+function myClick2(){
+    window.open('https://github.com/')
+}
